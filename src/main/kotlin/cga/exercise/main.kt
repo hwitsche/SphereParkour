a391aa002422b4365e0f6292da98d057382674a8
slauner <sandra.wanda_launer@smail.th-koeln.de>
2024-08-23 22:46:00 +0200

    In neue directory verschoben

--- src/main/kotlin/cga/exercise/main.kt
+++
@@ -1,8 +0,0 @@
-package cga.exercise
-
-import cga.exercise.game.Game
-
-fun main() {
-    val game = Game(1280, 720)
-    game.run()
-}
--- src/main/kotlin/cga/framework/RawMesh.kt
+++
@@ -1,6 +0,0 @@
-package cga.framework
-
-data class RawMesh(
-    var vertices: MutableList<Vertex> = mutableListOf(),
-    var indices: MutableList<Int> = mutableListOf(),
-    var materialIndex: Int = 0)
